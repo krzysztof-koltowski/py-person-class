1ca70cf22adedc3e21dc6493b52b086e4b4f12df
Krzysztof Kołtowski <kadis@o2.pl>
2025-06-23 16:31:27 +0200

    Solution v2

--- app/main.py
+++ app/main.py
@@ -1,29 +1,11 @@
-class Person:
-    people = {}  # Klasa atrybut do przechowywania wszystkich instancji
+from typing import Dict, List, Optional, Union
 
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        # Dodajemy instancję do słownika klasowego people
+class Person:
+    people: Dict[str, "Person"] = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name: str = name
+        self.age: int = age
         Person.people[name] = self
 
-
-def create_person_list(people_data):
-    # Pierwszy przebieg: tworzymy wszystkie osoby bez ustawiania związków małżeńskich
-    person_list = []
-    for person_dict in people_data:
-        name = person_dict["name"]
-        age = person_dict["age"]
-        person = Person(name, age)
-        person_list.append(person)
-
-    # Drugi przebieg: ustawiamy żony/mężów
-    for person_dict in people_data:
-        name = person_dict["name"]
-        person = Person.people[name]
-        if "wife" in person_dict and person_dict["wife"]:
-            person.wife = Person.people[person_dict["wife"]]
-        if "husband" in person_dict and person_dict["husband"]:
-            person.husband = Person.people[person_dict["husband"]]
-
-    return person_list
+def create_person_list(people_data: List[Dict[str, U]()]()_
